bf5be209a1e39a52540d06b00dee1826c144e881
Vitaliy Tyushin <vitaliy.tyushin@arcadia.spb.ru>
2021-12-19 16:32:55 +0300

    Apply missed renaming

--- src/app/top-albums/top-albums.module.ts
+++ src/app/top-albums/top-albums.module.ts
@@ -1,15 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { AlbumInfoModalService } from './album-info/album-info-modal.service';
+import { AlbumInfoModalComponent } from './album-info/album-info-modal/album-info-modal.component';
+import { AlbumListItemComponent } from './album-list/album-list-item/album-list-item.component';
+import { AlbumListComponent } from './album-list/album-list.component';
 import { TopAlbumsService } from './shared/top-albums.service';
 import { TopAlbumsRoutingModule } from './top-albums-routing.module';
 import { TopAlbumsComponent } from './top-albums.component';
-
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
-import { AlbumListItemComponent } from './album-list/album-list-item/album-list-item.component';
-import { AlbumListComponent } from './album-list/album-list.component';
-import { AlbumInfoModalServiceService } from './album-info/album-info-modal.service';
-import { MatDialogModule } from '@angular/material/dialog';
-import { AlbumInfoModalComponent } from './album-info/album-info-modal/album-info-modal.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +24,6 @@ import { AlbumInfoModalComponent } from './album-info/album-info-modal/album-inf
     MatDialogModule,
   ],
   exports: [TopAlbumsComponent],
-  providers: [TopAlbumsService, AlbumInfoModalServiceService],
+  providers: [TopAlbumsService, AlbumInfoModalService],
 })
 export class TopAlbumsModule {}
